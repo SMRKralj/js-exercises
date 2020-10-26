509e05d619bfb5910c371374db7faf8f8e2aaac9
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:36:14 -0300

    Exercise 2c

---
+++ strings/exercise-2c/js/exercise-2c.js
@@ -0,0 +1,7 @@
+/*
+ Create a string variable with at least 10 characters and generate 
+ a new string with the last 3 characters by saving the result to a new variable (use substring).
+ */
+var str = "Try to be a rainbow in someone's cloud";
+var subString = str.substring(35, 38);
+console.log(subString);
