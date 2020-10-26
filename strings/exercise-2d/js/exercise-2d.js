a93f763a4ea3f0b842b3c5da1f193fcd3db7fd8f
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:36:51 -0300

    Exercise 2d

---
+++ strings/exercise-2d/js/exercise-2d.js
@@ -0,0 +1,8 @@
+/*
+ Create a string variable with at least 10 characters and generate a new string 
+ with the first uppercase letter and the others lowercase. Save the result to a 
+ new variable (use substring, toUpperCase, toLowerCase, and the + operator).
+ */
+var str = "yOU CHANGE YOUR LIFE BY CHANGING YOUR HEART";
+var upperLowStr = str.substring(0, 1).toUpperCase() + str.substring(1, str.lenght).toLowerCase();
+console.log(upperLowStr);
