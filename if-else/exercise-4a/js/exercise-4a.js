3b800b3db619adc1d420f612b0f6f03859e60121
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:50:22 -0300

    Exercise 4a

---
+++ if-else/exercise-4a/js/exercise-4a.js
@@ -0,0 +1,11 @@
+/*
+  Create a random number between 0 and 1 using the Math.random() function, 
+  if the value is greater than or equal to 0.5 display an alert with the message "Greater than 0.5" 
+  and but an alert with the message "Lower than 0.5"
+ */
+
+if (Math.random() >= 0.5) {
+    alert("Greater than 0.5");
+} else {
+    alert("Lower than 0.5");
+}
