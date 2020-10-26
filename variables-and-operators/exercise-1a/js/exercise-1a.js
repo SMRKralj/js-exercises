0c6dd8c8dafad702dc6dd756108b77ab5e31c37a
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:23:08 -0300

    Exercise 1a

---
+++ variables-and-operators/exercise-1a/js/exercise-1a.js
@@ -0,0 +1,9 @@
+/* 
+ Variables and Operators
+ Create two numeric variables and use the sum operator to save 
+ the value of the sum of both numbers in a 3rd variable.
+ */
+var numOne = 456;
+var numTwo = 789;
+var sum = numOne + numTwo;
+console.log("The sum of the number " + numOne + " and the number " + numTwo + " is " + sum);
