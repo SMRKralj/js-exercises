f727e4488904d6987468759262fd620febdbc5dd
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:30:54 -0300

    Exercise 1b

---
+++ variables-and-operators/exercise-1b/js/exercise-1b.js
@@ -0,0 +1,8 @@
+/*
+ Variables and Operators
+ Create two variables of String type and concatenate them by saving the result in a 3rd variable
+ */
+var strOne = "Two halves of the";
+var strTwo = "same soul detached";
+var strThree = strOne.concat(" ",strTwo);
+console.log(strThree)
