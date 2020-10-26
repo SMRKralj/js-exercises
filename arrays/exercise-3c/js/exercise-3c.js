cf94c8bf871be24c3326a02d507ea61dd6d4e4a7
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:41:03 -0300

    Exercise 3c

---
+++ arrays/exercise-3c/js/exercise-3c.js
@@ -0,0 +1,7 @@
+/*
+ Add an element to the beginning and end of the array (use unshift and push).
+ */
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+months.unshift("Sachary");
+months.push("Covid");
+console.log(months.toString());
