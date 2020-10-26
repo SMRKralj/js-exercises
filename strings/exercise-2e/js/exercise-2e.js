70ffd18a53f04f099dddb6c9c24271686430a5c2
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:37:51 -0300

    Exercise 2e

---
+++ strings/exercise-2e/js/exercise-2e.js
@@ -0,0 +1,7 @@
+/*
+Create a variable of type string with at least 10 characters and some white space.
+Find the position of the first blank space and save it to a variable (use indexOf).
+*/
+var str = "Love is life. And if you miss love, you miss life";
+var pos = str.indexOf(" ");
+console.log("The first blank space is in the " + pos + "th position");
