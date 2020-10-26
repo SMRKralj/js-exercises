5cf3dc1ec38b8b0c5d07aa2b5ef10d9ee07fe2b2
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:42:23 -0300

    Exercise 3e

---
+++ arrays/exercise-3e/js/exercise-3e.js
@@ -0,0 +1,5 @@
+/*
+ Reverse array order (use reverse)
+ */
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(months.reverse().toString());
