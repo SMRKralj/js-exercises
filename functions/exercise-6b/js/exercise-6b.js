79f4fee0b8ae914663cc50925fae88081cd9d389
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:56:21 -0300

    Exercise 6b

---
+++ functions/exercise-6b/js/exercise-6b.js
@@ -0,0 +1,19 @@
+/*
+ To the previous sum function, add validation to control whether any of the parameters are not a number, 
+ display an alert clarifying that one of the parameters fails, and return the NaN value as a result.
+ */
+
+var numOne = parseFloat(prompt("ingrese el 1º número:"));
+var numTwo = parseFloat(prompt("ingrese el 2º número:"));
+var result = sum(numOne, numTwo);
+console.log("The result is " + result);
+
+
+function sum(numOne, numTwo) {
+    if (isNaN(numOne) || (isNaN(numTwo))) {
+        alert("One or both parameters have an error");
+        return NaN;
+    } else {
+        return numOne + numTwo;
+    }
+}
