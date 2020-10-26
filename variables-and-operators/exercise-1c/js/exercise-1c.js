61b070a97790f13abfc1b059f39fbd82b265da22
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:31:49 -0300

    Exercise 1c

---
+++ variables-and-operators/exercise-1c/js/exercise-1c.js
@@ -0,0 +1,12 @@
+/*
+ Variables and Operators
+
+Create two variables of String type and sum 
+the length of each variable (number of letters of the string)
+saving the result the sum a 3rd variable (use length).
+
+ */
+var strOne = "Two halves of the";
+var strTwo = "same soul detached";
+var strLengthSum = strOne.length + strTwo.length;
+console.log("The sum of the lengths of both strings is " + strLengthSum + " characters");
