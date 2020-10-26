caf771d613438825fb54cc41ee487b7baff417f5
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:49:15 -0300

    Exercise 3g

---
+++ arrays/exercise-3g/js/exercise-3g.js
@@ -0,0 +1,5 @@
+/*
+ Create a copy of the months array that contains from May to November (use slice).
+ */
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(months.slice(4, 11).toString())
