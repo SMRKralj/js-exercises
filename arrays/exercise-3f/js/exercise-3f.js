926f390655c8ab97455574b32e23420dd0664f68
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:48:39 -0300

    Exercise 3f

---
+++ arrays/exercise-3f/js/exercise-3f.js
@@ -0,0 +1,5 @@
+/*
+ Join all array elements into a single string where each month it is separated by a hyphen - (use join).
+ */
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+console.log(months.join("-"));
