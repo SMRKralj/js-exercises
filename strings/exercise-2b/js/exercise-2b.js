2b88b89833efc94c8b2e4b348ecdbe649c7c6e4e
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:35:35 -0300

    Exercise 2b

---
+++ strings/exercise-2b/js/exercise-2b.js
@@ -0,0 +1,7 @@
+/*
+ Create a string variable with at least 10 characters and  generate a new string
+  with the first 5 characters by saving the result to a new variable (use substring).
+ */
+var str = "Honesty is the first chapter in the book of wisdom";
+var subString = str.substring(0, 5);
+console.log(subString);
