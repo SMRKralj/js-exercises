db81b56fe38d745f978070141c9a73b6797e756a
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:34:53 -0300

    Exercise 2a

---
+++ strings/exercise-2a/js/exercise-2a.js
@@ -0,0 +1,6 @@
+/*
+ Create a string variable with at least 10 characters 
+ and convert all text to uppercase (use toUpperCase).
+ */
+var str = "Well done is better than well said";
+console.log(str.toUpperCase());
