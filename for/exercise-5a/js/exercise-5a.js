9bd49e416a78f7a9448196b658e9dbb5f0a21ce3
smrk <rodriguezsm72@gmail.com>
2020-10-26 09:51:57 -0300

    Exercise 5a

---
+++ for/exercise-5a/js/exercise-5a.js
@@ -0,0 +1,8 @@
+/*
+ Create an array containing 5 words and traverse that array using 
+ a JavaScript for loop to display an alert using each of the words.
+ */
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday"];
+for (let i = 0; i < days.length; i++) {
+    alert(days[i].toString());
+}
