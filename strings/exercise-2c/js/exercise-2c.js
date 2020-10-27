b9fce42f2d667256ce782f6deb8b308c7bbedee2
smrk <rodriguezsm72@gmail.com>
2020-10-26 22:40:13 -0300

    Exercise 2c with length

--- strings/exercise-2c/js/exercise-2c.js
+++ strings/exercise-2c/js/exercise-2c.js
@@ -3,5 +3,5 @@
  a new string with the last 3 characters by saving the result to a new variable (use substring).
  */
 var str = "Try to be a rainbow in someone's cloud";
-var subString = str.substring(35, 38);
+var subString = str.substring(str.length, str.length - 3);
 console.log(subString);
