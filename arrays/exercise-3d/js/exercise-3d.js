7dc850772292dd38164b922cb31cc402676a99dc
smrk <rodriguezsm72@gmail.com>
2020-10-26 22:52:32 -0300

    Exercise 3d

--- arrays/exercise-3d/js/exercise-3d.js
+++ arrays/exercise-3d/js/exercise-3d.js
@@ -4,4 +4,4 @@
 var months = ["Sachary", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "Covid"];
 var delAtStart = months.shift();
 var delAtEnd = months.pop();
-console.log("The element removed at the beginning of the array is " + delAtStart + " and the element removed at the end is " + delAtEnd);
+console.log(months);
