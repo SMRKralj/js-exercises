23b2b9623423465910c5a1c93b2a5daa299fbecc
smrk <rodriguezsm72@gmail.com>
2020-10-26 22:47:43 -0300

    Exercise 3b without to string

--- arrays/exercise-3b/js/exercise-3b.js
+++ arrays/exercise-3b/js/exercise-3b.js
@@ -2,4 +2,4 @@
  Sort the array of months alphabetically and display it by console (use sort).
 */
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-console.log(months.sort().toString());
+console.log(months.sort());
